f331cfa9f7d625f8e8bee2e431484a7d00859438
T.U <takayo.uenter36@icloud.com>
2025-02-26 02:28:15 +0900

    ✨ とりあえずGraphQL使えるように (#4)
    * add hotchocolate middleware
    
    * add graphql

---
+++ EcSiteBackend/src/EcSiteBackend.Presentation/EcSiteBackend.WebAPI/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace EcSiteBackend.Presentation.EcSiteBackend.WebAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>();
+        }
+        
+        // 仮置き
+        private class Query
+        {
+            public string Hello() => "Hello, GraphQL!";
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL(); // GraphQLエンドポイントをマッピング
+            });
+        }
+    }
+}
