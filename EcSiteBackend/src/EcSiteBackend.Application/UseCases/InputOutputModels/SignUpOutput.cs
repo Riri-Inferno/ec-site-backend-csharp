d596cbca60a7aade40f8fdbd75285aec4278d55b
T.U <takayo.uenter36@icloud.com>
2025-07-04 03:10:41 +0900

    適当な差分

--- EcSiteBackend/src/EcSiteBackend.Application/UseCases/InputOutputModels/SignUpOutput.cs
+++ EcSiteBackend/src/EcSiteBackend.Application/UseCases/InputOutputModels/SignUpOutput.cs
@@ -5,7 +5,7 @@ namespace EcSiteBackend.Application.UseCases.InputOutputModels
     /// <summary>
     /// SignUp時の出力モデル
     /// </summary>
-    public class SignUpOutput
+    public class SignUpOutput //
     {
         public UserDto User { get; set; } = null!;
         public string Token { get; set; } = string.Empty;
