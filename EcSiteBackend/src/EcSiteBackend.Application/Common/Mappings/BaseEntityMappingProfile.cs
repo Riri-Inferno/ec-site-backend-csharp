0d7091b2fe6435ce54129006d4b9523f14e6968c
T.U <takayo.uenter36@icloud.com>
2025-07-09 00:39:49 +0900

    監査エンティティ自動設定とマッピング設定を追加

---
+++ EcSiteBackend/src/EcSiteBackend.Application/Common/Mappings/BaseEntityMappingProfile.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using EcSiteBackend.Domain.Entities;
+
+namespace EcSiteBackend.Application.Common.Mappings
+{
+    /// <summary>
+    /// すべてのBaseEntityに共通するマッピング設定
+    /// </summary>
+    public class BaseEntityMappingProfile : Profile
+    {
+        public BaseEntityMappingProfile()
+        {
+            // すべてのBaseEntity派生クラスに適用される共通設定
+            CreateMap<BaseEntity, BaseEntity>()
+                .IncludeAllDerived()
+                .ForMember(dest => dest.RowVersion, opt => opt.Ignore());
+        }
+    }
+}
