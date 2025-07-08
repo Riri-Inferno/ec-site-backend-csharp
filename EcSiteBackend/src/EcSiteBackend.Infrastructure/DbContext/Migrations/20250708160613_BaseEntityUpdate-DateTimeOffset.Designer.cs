29b3f517d4b58b420673db059d0e785e0aaf7c2b
T.U <takayo.uenter36@icloud.com>
2025-07-09 01:06:36 +0900

    マイグレファイルを生成

--- EcSiteBackend/src/EcSiteBackend.Infrastructure/DbContext/Migrations/20250708160613_BaseEntityUpdate-DateTimeOffset.Designer.cs
+++ EcSiteBackend/src/EcSiteBackend.Infrastructure/DbContext/Migrations/20250708160613_BaseEntityUpdate-DateTimeOffset.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EcSiteBackend.Infrastructure.DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EcSiteBackend.Infrastructure.DbContext.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250708160613_BaseEntityUpdate-DateTimeOffset")]
+    partial class BaseEntityUpdateDateTimeOffset
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EcSiteBackend/src/EcSiteBackend.Infrastructure/DbContext/Migrations/20250708160613_BaseEntityUpdate-DateTimeOffset.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcSiteBackend.Infrastructure.DbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaseEntityUpdateDateTimeOffset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
