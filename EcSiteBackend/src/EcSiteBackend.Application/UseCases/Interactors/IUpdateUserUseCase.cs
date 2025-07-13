744cd77100a0ccfa585d7c5eb824aedf7036f5ef
T.U <takayo.uenter36@icloud.com>
2025-07-13 21:21:34 +0900

    ユースケースのIFを追加

---
+++ EcSiteBackend/src/EcSiteBackend.Application/UseCases/Interactors/IUpdateUserUseCase.cs
@@ -0,0 +1,17 @@
+using EcSiteBackend.Application.UseCases.InputOutputModels;
+
+namespace EcSiteBackend.Application.UseCases.Interfaces
+{
+    /// <summary>
+    /// ユーザー更新ユースケースのインターフェース
+    /// </summary>
+    public interface IUpdateUserUseCase
+    {
+        /// <summary>
+        /// ユーザー情報を更新する
+        /// </summary>
+        /// <param name="input">更新するユーザー情報</param>
+        /// <param name="cancellationToken">キャンセルトークン</param>
+        Task ExecuteAsync(UpdateUserInput input, CancellationToken cancellationToken);
+    }
+}
