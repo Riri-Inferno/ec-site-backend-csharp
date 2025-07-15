9ff55d4a98d64be632b87f970588d362e6ae14da
Riri-Inferno <takayo.uenter36@icloud.com>
2025-07-15 13:02:15 +0900

    実装予定ケースの空テストケースを追加

--- EcSiteBackend/tests/EcSiteBackend.UnitTests/Interactors/ChangePasswordInteractorTests.cs
+++ EcSiteBackend/tests/EcSiteBackend.UnitTests/Interactors/ChangePasswordInteractorTests.cs
@@ -50,5 +50,96 @@ namespace EcSiteBackend.UnitTests.Interactors
                 _loggerMock.Object
             );
         }
+
+        [Fact(DisplayName = "正常系:有効な現在のパスワードで新しいパスワードに変更できること")]
+        public async Task ChangePassword_ValidCurrentPassword_ShouldChangePasswordSuccessfully()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "正常系:パスワード変更時にユーザー履歴が作成されること")]
+        public async Task ChangePassword_ShouldCreateUserHistory()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "正常系:パスワード変更成功時にログイン履歴が記録されること")]
+        public async Task ChangePassword_ShouldCreateLoginHistoryOnSuccess()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "正常系:トランザクションが正常に実行されること")]
+        public async Task ChangePassword_ShouldExecuteWithinTransaction()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "正常系:ユーザーの監査情報（UpdatedAt, UpdatedBy）が更新されること")]
+        public async Task ChangePassword_ShouldUpdateAuditFields()
+        {
+            // TODO: 実装
+        }
+
+        // 異常系
+        [Fact(DisplayName = "異常系:新しいパスワードと確認パスワードが一致しない場合、ValidationExceptionがスローされること")]
+        public async Task ChangePassword_NewPasswordAndConfirmNotMatch_ShouldThrowValidationException()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "異常系:必須項目（CurrentPassword, NewPassword）が空の場合、ValidationExceptionがスローされること")]
+        public async Task ChangePassword_RequiredFieldsEmpty_ShouldThrowValidationException()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "異常系:UserIdが空のGUIDの場合、InvalidArgumentsExceptionがスローされること")]
+        public async Task ChangePassword_EmptyGuid_ShouldThrowInvalidArgumentsException()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "異常系:ユーザーが存在しない場合、NotFoundExceptionがスローされること")]
+        public async Task ChangePassword_UserNotFound_ShouldThrowNotFoundException()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "異常系:ユーザーが非アクティブの場合、NotFoundExceptionがスローされること")]
+        public async Task ChangePassword_UserInactive_ShouldThrowNotFoundException()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "異常系:現在のパスワードが間違っている場合、UnauthorizedExceptionがスローされること")]
+        public async Task ChangePassword_CurrentPasswordInvalid_ShouldThrowUnauthorizedException()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "異常系:新しいパスワードが現在のパスワードと同じ場合、ValidationExceptionがスローされること")]
+        public async Task ChangePassword_NewPasswordSameAsCurrent_ShouldThrowValidationException()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "異常系:新しいパスワードが弱い場合、ValidationExceptionがスローされること")]
+        public async Task ChangePassword_WeakNewPassword_ShouldThrowValidationException()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "異常系:パスワード検証失敗時に失敗ログイン履歴が記録されること")]
+        public async Task ChangePassword_VerifyPasswordFailed_ShouldCreateFailedLoginHistory()
+        {
+            // TODO: 実装
+        }
+
+        [Fact(DisplayName = "異常系:トランザクション内で例外が発生した場合、ロールバックされること")]
+        public async Task ChangePassword_ExceptionInTransaction_ShouldRollback()
+        {
+            // TODO: 実装
+        }
     }
 }
